536b35440bfe674b6710ebf783c4ecbb22352875
姜皜 <francis1572@gmail.com>
2019-12-15 17:22:05 +0800

    fix init

--- uncleproject/uncleproject/settings.py
+++ uncleproject/uncleproject/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'explore.apps.ExploreConfig',
-    'django_mysql'
+    'explore.apps.ExploreConfig'
 ]
 
 MIDDLEWARE = [
